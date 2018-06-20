ed19f956a58476d4517925c8f6f62291c13c8f44
mlawrence <mlawrence@alsde.edu>
2018-06-20 16:27:34 -0500

    Implement basic site api controller returning site list

---
+++ EdDirSites.Web/Controllers/Api/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using EdDirSites.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EdDirSites.Web.Controllers.Api
+{
+    public class SiteController : ApiController
+    {
+        private EdContext context;
+
+        public SiteController()
+        {
+            context = new EdContext();
+        }
+
+        public async Task<object> Get()
+        {
+            return Ok(context.Sites.OrderBy(x => x.Id).Skip(0).Take(100).ToList());
+        }
+    }
+}
