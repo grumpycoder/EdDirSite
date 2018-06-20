41d100a0e4c00bee78d2ad3d65fb66d3bf704a69
mlawrence <mlawrence@alsde.edu>
2018-06-20 16:33:38 -0500

    implement get and get with id param on site controller

--- EdDirSites.Web/Controllers/Api/SiteController.cs
+++ EdDirSites.Web/Controllers/Api/SiteController.cs
@@ -1,10 +1,12 @@
 ﻿using EdDirSites.Core.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace EdDirSites.Web.Controllers.Api
 {
+    [RoutePrefix("api/site")]
     public class SiteController : ApiController
     {
         private EdContext context;
@@ -16,7 +18,15 @@ namespace EdDirSites.Web.Controllers.Api
 
         public async Task<object> Get()
         {
-            return Ok(context.Sites.OrderBy(x => x.Id).Skip(0).Take(100).ToList());
+            return Ok(await context.Sites.OrderBy(x => x.Id).Skip(0).Take(100).ToListAsync());
+        }
+
+        [HttpGet, Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            var site = await context.Sites.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(site);
         }
     }
 }
