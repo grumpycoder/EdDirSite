0d18e7edb29eee2bd4b8d8509cb246b2bce4ddbf
mlawrence <mlawrence@alsde.edu>
2018-06-21 08:17:09 -0500

    implement web api method to return single or list of sites

--- EdDirSites.Web/Controllers/Api/SiteController.cs
+++ EdDirSites.Web/Controllers/Api/SiteController.cs
@@ -21,12 +21,27 @@ namespace EdDirSites.Web.Controllers.Api
             return Ok(await context.Sites.OrderBy(x => x.Id).Skip(0).Take(100).ToListAsync());
         }
 
-        [HttpGet, Route("{id}")]
-        public async Task<object> Get(int id)
-        {
-            var site = await context.Sites.FirstOrDefaultAsync(x => x.Id == id);
+        //[HttpGet, Route("{id:int}")]
+        //public async Task<object> Get(int id)
+        //{
+        //    var site = await context.Sites.FirstOrDefaultAsync(x => x.Id == id);
 
-            return Ok(site);
+        //    return Ok(site);
+        //}
+
+        [HttpGet, Route("{syscode}/{sitecode?}")]
+        public async Task<object> Get(string syscode, string sitecode = null)
+        {
+            if (!string.IsNullOrEmpty(sitecode))
+            {
+                var site = await context.Sites.FirstOrDefaultAsync(x => x.SysCode.ToString() == syscode && x.SiteCode == sitecode);
+                return Ok(site);
+            }
+
+            var list = await context.Sites.Where(x => x.SysCode == syscode).ToListAsync();
+
+            return Ok(list);
         }
+
     }
 }
